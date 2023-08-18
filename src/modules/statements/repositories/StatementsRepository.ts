331c1bd2f1d9cc1afc45f89b92caf7f73b66139d
Leonardo Deomedesse <53792927+leodeomedesse@users.noreply.github.com>
2023-08-17 22:22:03 -0400

    desafio11

---
+++ src/modules/statements/repositories/StatementsRepository.ts
@@ -0,0 +1,86 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateStatementDTO } from "../useCases/createStatement/ICreateStatementDTO";
+import { IGetBalanceDTO } from "../useCases/getBalance/IGetBalanceDTO";
+import { IGetStatementOperationDTO } from "../useCases/getStatementOperation/IGetStatementOperationDTO";
+import { IStatementsRepository } from "./IStatementsRepository";
+import { OperationType, Statement } from "../entities/Statement";
+
+export class StatementsRepository implements IStatementsRepository {
+  private repository: Repository<Statement>;
+
+  constructor() {
+    this.repository = getRepository(Statement);
+  }
+
+  async create({
+    user_id,
+    sender_id,
+    amount,
+    description,
+    type
+  }: ICreateStatementDTO): Promise<Statement> {
+    if (type === 'transfer') {
+      const senderStatement = this.repository.create({
+        user_id: sender_id,
+        amount,
+        description,
+        type
+      });
+
+      await this.repository.save(senderStatement);
+
+      const receiverStatement = this.repository.create({
+        user_id,
+        sender_id,
+        amount,
+        description,
+        type
+      });
+
+      return this.repository.save(receiverStatement);
+    }
+
+    const statement = this.repository.create({
+      user_id,
+      amount,
+      description,
+      type
+    });
+
+    return this.repository.save(statement);
+  }
+
+  async findStatementOperation({ statement_id, user_id }: IGetStatementOperationDTO): Promise<Statement | undefined> {
+    return this.repository.findOne(statement_id, {
+      where: { user_id }
+    });
+  }
+
+  async getUserBalance({ user_id, with_statement = false }: IGetBalanceDTO):
+    Promise<
+      { balance: number } | { balance: number, statement: Statement[] }
+    >
+  {
+    const statement = await this.repository.find({
+      where: { user_id }
+    });
+
+    const balance = statement.reduce((acc, operation) => {
+      if (operation.type === 'deposit') {
+        return acc + operation.amount;
+      } else {
+        return acc - operation.amount;
+      }
+    }, 0)
+
+    if (with_statement) {
+      return {
+        statement,
+        balance
+      }
+    }
+
+    return { balance }
+  }
+}
